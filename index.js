b0457f75b1772acca528fe85442143fc102d818a
Hamza <hamzaammar2@yahoo.com>
2023-09-07 02:24:26 +0300

    links

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const cont = document.querySelector(".proj-cont");
 
 const projects = [
-    createProj("Facebook", "assets/fbuser-desk.png", "", ""),
-    createProj("Chess-site", "assets/ch-matches.png", "", ""),
-    createProj("Camp-website", "assets/yelp--main.png", "", ""),
-    createProj("Connect4", "assets/connect4.png", "", ""),
-    createProj("Ecommerce-site", "assets/ecommerce.png", "", ""),
-    createProj("Todo-app", "assets/todo.png", "", "")
+    createProj("Facebook", "assets/fbuser-desk.png", "https://github.com/Hamza-Ammar2/Mini-book", "https://minibook.onrender.com"),
+    createProj("Chess-site", "assets/ch-matches.png", "https://github.com/Hamza-Ammar2/Chess-site", "https://chess-845m.onrender.com"),
+    createProj("Camp-website", "assets/yelp--main.png", "https://github.com/Hamza-Ammar2/Camp-site", "https://hamza-ammar2.github.io/Camp-site/"),
+    createProj("Connect4", "assets/connect4.png", "https://github.com/Hamza-Ammar2/Connect4", "https://hamza-ammar2.github.io/Connect4/"),
+    createProj("Ecommerce-site", "assets/ecommerce.png", "https://github.com/Hamza-Ammar2/ecommerce", "https://hamza-ammar2.github.io/ecommerce/"),
+    createProj("Todo-app", "assets/todo.png", "https://github.com/Hamza-Ammar2/todo", "https://hamza-ammar2.github.io/todo/")
 ];
 
 
