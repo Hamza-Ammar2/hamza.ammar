d2a43736462032f73b868f4d580cb5bbdaa36cc6
Hamza <hamzaammar2@yahoo.com>
2023-09-26 00:03:36 +0300

    link fix

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cont = document.querySelector(".proj-cont");
 const projects = [
     createProj("Facebook", "assets/fbuser-desk.png", "https://github.com/Hamza-Ammar2/Mini-book", "https://minibook.onrender.com"),
     createProj("Chess-site", "assets/ch-matches.png", "https://github.com/Hamza-Ammar2/Chess-site", "https://chess-845m.onrender.com"),
-    createProj("Camp-website", "assets/yelp--main.png", "https://github.com/Hamza-Ammar2/Camp-site", "https://hamza-ammar2.github.io/Camp-site/"),
+    createProj("Camp-website", "assets/yelp--main.png", "https://github.com/Hamza-Ammar2/Camp-website", "https://hamza-ammar2.github.io/Camp-website/"),
     createProj("Connect4", "assets/connect4.png", "https://github.com/Hamza-Ammar2/Connect4", "https://hamza-ammar2.github.io/Connect4/"),
     createProj("Ecommerce-site", "assets/ecommerce.png", "https://github.com/Hamza-Ammar2/ecommerce", "https://hamza-ammar2.github.io/ecommerce/"),
     createProj("Todo-app", "assets/todo.png", "https://github.com/Hamza-Ammar2/todo", "https://hamza-ammar2.github.io/todo/")
